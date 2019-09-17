9d3b1da8d06c6dee1d3c4c5ae21a47687b1ad199
ve-agui <1084424111@qq.com>
2019-09-17 10:19:05 +0800

    修改查詢條件

--- mall-platform/src/main/java/com/perenc/mall/platform/service/impl/BannerServiceImpl.java
+++ mall-platform/src/main/java/com/perenc/mall/platform/service/impl/BannerServiceImpl.java
@@ -32,8 +32,6 @@ public class BannerServiceImpl extends BaseService<BannerMapper, BannerDO> imple
     @Override
     public List<BannerDO> listBanners() {
         QueryWrapper<BannerDO> queryWrapper = new QueryWrapper<>();
-        // 查询所有可用的轮播图
-        queryWrapper.eq("is_enable", 0);
         // 进行升序排序
         queryWrapper.orderByAsc("sort");
         List<BannerDO> listBanner = super.listEntitys(queryWrapper);
