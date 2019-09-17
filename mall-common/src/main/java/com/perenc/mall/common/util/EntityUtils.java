b07358b155e0778c8c4d5b9dfd1788dc8e1bf0da
ve-agui <1084424111@qq.com>
2019-09-17 14:38:56 +0800

    实体类工具

--- mall-common/src/main/java/com/perenc/mall/common/util/EntityUtils.java
+++ mall-common/src/main/java/com/perenc/mall/common/util/EntityUtils.java
@@ -2,14 +2,7 @@ package com.perenc.mall.common.util;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.Validate;
 import org.springframework.util.Assert;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
